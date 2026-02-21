7797ff8299f7010fb585bb59d54a8acfd1d19d84
SiriusSeptimus <tombo@LAPTOP-KLULIUKI>
2026-02-20 22:07:30 -0500

    Finished up settings menu and added full-screen capacity

--- main_menu.gd
+++ main_menu.gd
@@ -13,3 +13,19 @@ func _process(delta: float) -> void:
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://settings.tscn") # Replace with function body.
+
+
+
+func _on_return_to_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
+func _on_full_screen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else: 
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_exit_game_pressed() -> void:
+	get_tree().quit()
