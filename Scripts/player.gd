506817e1c4b1bffa0fcca6886c508832b91af148
Pvt_Murphys <joshpoteet@icloud.com>
2026-02-26 15:32:55 -0500

    Interact capability created
    Code framework built to allow for dynamic interaction direction

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,36 @@
 extends CharacterBody2D
-signal collide
+signal interact
 @export var speed = 400
 var screen_size
+var activebody
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size 
 
+func change_interactable(velocity):
+	if velocity.x > 0:
+		$InteractBodyRight.show()
+		$InteractBodyLeft.hide()
+		$InteractBodyDown.hide()
+		$InteractBodyUp.hide()
+	if velocity.x < 0:
+		$InteractBodyRight.hide()
+		$InteractBodyLeft.show()
+		$InteractBodyDown.hide()
+		$InteractBodyUp.hide()
+	if velocity.x == 0:
+		if velocity.y > 0:
+			$InteractBodyRight.hide()
+			$InteractBodyLeft.hide()
+			$InteractBodyDown.show()
+			$InteractBodyUp.hide()
+		if velocity.y < 0:
+			$InteractBodyRight.hide()
+			$InteractBodyLeft.hide()
+			$InteractBodyDown.hide()
+			$InteractBodyUp.show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func get_input():
@@ -22,25 +45,46 @@ func get_input():
 	else:
 		$AnimatedSprite2D.stop()
 	
-	if velocity.x != 0 && velocity.y == 0:
+	if velocity.x > 0 && velocity.y == 0:
 		$AnimatedSprite2D.animation = "walk_horiz"
-		$AnimatedSprite2D.flip_h = velocity.x < 0
+		$AnimatedSprite2D.flip_h = false
+		change_interactable(velocity)
+	if velocity.x < 0 && velocity.y == 0:
+		$AnimatedSprite2D.animation = "walk_horiz"
+		$AnimatedSprite2D.flip_h = true
+		change_interactable(velocity)
 	if velocity.y > 0 && velocity.x == 0:
 		$AnimatedSprite2D.animation = "walk_down"
+		change_interactable(velocity)
 	if velocity.y < 0 && velocity.x == 0:
 		$AnimatedSprite2D.animation = "walk_up"
+		change_interactable(velocity)
 	if velocity.y < 0 && velocity.x > 0:
 		$AnimatedSprite2D.animation = "walk_updiag"
 		$AnimatedSprite2D.flip_h = false
+		change_interactable(velocity)
 	if velocity.y < 0 && velocity.x < 0:
 		$AnimatedSprite2D.animation = "walk_updiag"
 		$AnimatedSprite2D.flip_h = true
+		change_interactable(velocity)
 	if velocity.y > 0 && velocity.x > 0:
 		$AnimatedSprite2D.animation = "walk_downdiag"
 		$AnimatedSprite2D.flip_h = false
+		change_interactable(velocity)
 	if velocity.y > 0 && velocity.x < 0:
 		$AnimatedSprite2D.animation = "walk_downdiag"
 		$AnimatedSprite2D.flip_h = true
+		change_interactable(velocity)
+	if velocity.x > 0:
+		activebody = $InteractBodyRight
+	elif velocity.x < 0:
+		activebody = $InteractBodyLeft
+	elif velocity.y > 0:
+		activebody = $InteractBodyDown
+	elif velocity.y < 0:
+		activebody = $InteractBodyUp
+		
+	
 		
 
 
@@ -57,10 +101,5 @@ func _process(delta):
 	if Input.is_key_pressed(KEY_B):
 		var game_manager = get_node("%GameManager")
 		game_manager.switch_to_battle()
-	
-	
-
-
-func _on_body_entered(body: Node2D) -> void:
-	collide.emit()
-	
+	if Input.is_action_just_pressed("Interact"):
+		print(activebody)
