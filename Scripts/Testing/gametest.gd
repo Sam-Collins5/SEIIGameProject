2f292ec67cbf0174bea68a9f38829b105d14cdbb
SiriusSeptimus <tombo@LAPTOP-KLULIUKI>
2026-04-21 12:20:11 -0400

    Finished up a proper test on the healing potion functionality.

--- Scripts/Testing/gametest.gd
+++ Scripts/Testing/gametest.gd
@@ -38,7 +38,7 @@ func test_battle() -> void:
 	take_damage_test()
 	question_damage_test()
 	deal_damage_test()
-	#healing_potion_test()
+	healing_potion_test()
 	if errors.is_empty():
 		print("Tests passed!")
 		quit(0)
@@ -84,26 +84,38 @@ func deal_damage_test() -> void:
 	battle.enemy.health_points -= battle.player.attack_power
 	if battle.enemy.health_points < 0:
 		battle.enemy.health_points = 0
-	assert_equal(battle.enemy.health_points, 9, "Playr turn: player attacking enemy should decrease enemy HP by attack_powe", "Deal Damage test successful!")
+	assert_equal(battle.enemy.health_points, 9, "Playr turn: player attacking enemy should decrease enemy HP by attack_power", "Deal Damage test successful!")
 	
 	battle.player.free()
 	battle.enemy.free()
 	battle.free()
 
-#func healing_potion_test() -> void:
-	#var battle = BATTLE_MANAGER.new()
-	#var items = BATTLE_ITEMS.new()
-	#items.item_ui = Control.new()
-	#items.battle_manager = battle
-	#
-	#battle.player = test_player(20, 3, 3.0)
-	#battle.enemy = test_enemy(12, 2)
-	#
-	#print(battle.player.health_points)
-	#items._on_health_pressed()
-	#print(battle.player.health_points)
-	#
-	#battle.player.free()
-	#battle.enemy.free()
-	#battle.free()
-	#
+func healing_potion_test() -> void:
+	var root_node = Node.new()
+	get_root().add_child(root_node)
+	var battle = BATTLE_MANAGER.new()
+	var items = BATTLE_ITEMS.new()
+	items.item_ui = Control.new()
+	items.battle_manager = battle
+	items.popup_label = Label.new()
+	items.popup_label.text = "test"
+	items.popup = Control.new()
+	items.popup_timer = Timer.new()
+	root_node.add_child(items.popup_timer)
+	
+	battle.player = test_player(20, 3, 3.0)
+	battle.enemy = test_enemy(12, 2)
+	
+	items._on_health_pressed()
+	assert_equal(battle.player.health_points, 40, "Player turn: player using healing potion should increase player HP by 20", "Healing Potion test successful!")
+	
+	battle.player.free()
+	battle.enemy.free()
+	battle.free()
+	items.popup.free()
+	items.popup_label.free()
+	items.popup_timer.free()
+	items.item_ui.free()
+	items.free()
+	root_node.queue_free()
+	
